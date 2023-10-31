0af129b4e7ef5b9f1c8599cfe960f263fdc9311c
xKendoVul <hadyihernandez@gmail.com>
2023-10-31 15:11:41 -0600

    actualizando
    :)

--- funcionalidad.js
+++ funcionalidad.js
@@ -27,11 +27,11 @@ let bookListContainer = document.getElementById('lista-libros');
 books.forEach(book => {
     let bookItem = document.createElement('div');
     bookItem.innerHTML = `
-    <img src="${books.portada}" alt="${books.titulo}" style="max-width: 100px; max-height: 100px;">
-        <h2>${books.titulo}</h2>
-        <p><strong>Autor:</strong> ${books.autor}</p>
-        <p><strong>Género:</strong> ${books.genero}</p>
-        <p><strong>Año:</strong> ${books.año}</p>x   
+        <h2>${book.titulo}</h2>
+        <img src="${book.portada}" alt="${book.titulo}" style="max-width: 100px; max-height: 100px;">
+        <p><strong>Autor:</strong> ${book.autor}</p>
+        <p><strong>Género:</strong> ${book.genero}</p>
+        <p><strong>Año:</strong> ${book.año}</p>
         <hr>
     `;
     bookListContainer.appendChild(bookItem);
@@ -41,7 +41,31 @@ const formulario = document.getElementById("add-book-form");
 
 formulario.addEventListener('submit', function (event) {
     event.preventDefault();
-    const titulo = document.getElementById("title");
-    const
+    const titulo = document.getElementById("title").value;
+    const autor = document.getElementById("author").value;
+    const genero = document.getElementById("genre").value;
+    const año = document.getElementById("year").value;
+    const imagen = document.getElementById("image").value;
 
+    books.push({ titulo, autor, genero, año, imagen });
+    formulario.reset();
+    displayBooks();
 })
+
+function displayBooks() {
+    bookListContainer.innerHTML = '';
+    books.forEach(book => {
+        let bookItem = document.createElement('div');
+        bookItem.innerHTML = `
+            <h2>${book.titulo}</h2>
+            <img src="${book.portada}" alt="${book.titulo}" style="max-width: 100px; max-height: 100px;">
+            <p><strong>Autor:</strong> ${book.autor}</p>
+            <p><strong>Género:</strong> ${book.genero}</p>
+            <p><strong>Año:</strong> ${book.año}</p>
+            <hr>
+        `;
+        bookListContainer.appendChild(bookItem);
+    });
+}
+
+displayBooks();
