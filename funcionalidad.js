a8ece972ee755e79b2e753d3f41b67f5790d4d8c
xKendoVul <hadyihernandez@gmail.com>
2023-10-30 22:17:23 -0600

    reagregando json
    :)

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,38 +1,32 @@
-let books = [
-    {
-        title: 'Cien años de soledad',
-        author: 'Gabriel García Márquez',
-        genre: 'Ficción',
-        year: 1967,
-        port: 'images/senor-de-los-anillos.jpeg'
-    },
-    {
-        title: 'El señor de los anillos',
-        author: 'J.R.R. Tolkien',
-        genre: 'Fantasía',
-        year: 1954,
-        port: 'images/senor-de-los-anillos.jpeg'
-    },
-    {
-        title: '1984',
-        author: 'George Orwell',
-        genre: 'Ciencia ficción',
-        year: 1949,
-        port: 'images/senor-de-los-anillos.jpeg'
-    }
-];
+fetch('json/lista-libros.json')
+    .then(response => response.json())
+    .then(data => {
+        // Trabajar con los datos obtenidos
+        console.log(data); // Mostrar los datos en la consola
+        displayBooks(data); // Llamar a la función para mostrar los libros en la página
+    })
+    .catch(error => console.error('Error al obtener los datos:', error));
 
-let bookListContainer = document.getElementById('book-list');
+function displayBooks(books) {
+    // Obtener el contenedor de la lista de libros
+    const bookListContainer = document.getElementById('lista-libros');
 
-books.forEach(book => {
-    let bookItem = document.createElement('div');
-    bookItem.innerHTML = `
-    <img src="${book.port}" alt="${book.title}" style="max-width: 100px; max-height: 100px;">
-        <h2>${book.title}</h2>
-        <p><strong>Autor:</strong> ${book.author}</p>
-        <p><strong>Género:</strong> ${book.genre}</p>
-        <p><strong>Año:</strong> ${book.year}</p>
-        <hr>
-    `;
-    bookListContainer.appendChild(bookItem);
-});
+    // Mostrar la lista de libros en el contenedor
+    books.forEach(book => {
+        let bookItem = document.createElement('div');
+        bookItem.innerHTML = `
+                <h2>${book.title}</h2>
+                <img src="${book.image}" alt="${book.title}" style="max-width: 100px; max-height: 100px;">
+                <p><strong>Autor:</strong> ${book.author}</p>
+                <p><strong>Género:</strong> ${book.genre}</p>
+                <p><strong>Año:</strong> ${book.year}</p>
+                <hr>
+            `;
+        bookListContainer.appendChild(bookItem);
+    });
+}
+
+
+
+
+
