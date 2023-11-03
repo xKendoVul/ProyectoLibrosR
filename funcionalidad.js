3b821c5f27a92c3ba3db193c422d6ddc29eade2b
xKendoVul <hadyihernandez@gmail.com>
2023-11-02 23:57:51 -0600

    Reajustes

--- funcionalidad.js
+++ funcionalidad.js
@@ -7,47 +7,53 @@ let libros = [
     }
 ];
 
+//*--------------------------------------------------------------
+
 let bookListContainer = document.getElementById('lista-libros');
 
-libros.forEach(libro => {
-    let bookItem = document.createElement('div');
-    bookItem.innerHTML = `
+function displayBooks() {
+    let coleccionLibros = JSON.parse(localStorage.getItem("biblioteca"))
+    coleccionLibros.forEach(libro => {
+        let bookItem = document.createElement('div');
+        bookItem.innerHTML = `
         <h2>${libro.titulo}</h2>
         <p><strong>Autor:</strong> ${libro.autor}</p>
         <p><strong>Género:</strong> ${libro.genero}</p>
         <p><strong>Año:</strong> ${libro.año}</p>
         <hr>
     `;
-    bookListContainer.appendChild(bookItem);
-});
-
-const formulario = document.getElementById("formulario");
-formulario.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const titulo = document.getElementById('title').value;
-    const autor = document.getElementById('author').value;
-    const genero = document.getElementById('genre').value;
-    const año = document.getElementById('year').value;
-    // let imagen = document.getElementById('image').value;
-    console.log(titulo, autor, genero, año)
-
-    localStorage.setItem('books', JSON.stringify(books));
-    formulario.reset();
-    displayBooks();
-});
-
-function displayBooks() {
-    libros.forEach(libro => {
-        let bookItem = document.createElement('div');
-        bookItem.innerHTML = `
-            <h2>${libro.titulo}</h2>
-            
-            <p><strong>Autor:</strong> ${libro.autor}</p>
-            <p><strong>Género:</strong> ${libro.genero}</p>
-            <p><strong>Año:</strong> ${libro.año}</p>
-            <hr>
-        `;
+        bookListContainer.appendChild(bookItem);
     });
 }
 
-displayBooks();
+//*--------------------------------------------------------------
+
+displayBooks()
+
+//*--------------------------------------------------------------
+
+const formulario = document.getElementById("formulario");
+formulario.addEventListener('submit', function (event) {
+    let biblioteca = JSON.parse(localStorage.getItem("biblioteca"))
+    event.preventDefault();
+    const tituloInput = document.getElementById('title').value;
+    const autorInput = document.getElementById('author').value;
+    const generoInput = document.getElementById('genre').value;
+    const añoInput = document.getElementById('year').value;
+    // let imagen = document.getElementById('image').value;
+    console.log(tituloInput, autorInput, generoInput, añoInput)
+
+    biblioteca.push({
+        titulo: tituloInput,
+        autor: autorInput,
+        genero: generoInput,
+        año: añoInput
+    });
+
+    bookListContainer.innerHTML = ''
+    formulario.reset();
+    localStorage.setItem("biblioteca", JSON.stringify(biblioteca))
+    displayBooks();
+});
+
+//*--------------------------------------------------------------
