14d29a75d98eb5d0f5261e1e5648e0cbd8865ff7
Mauricio Torrez <torrezmauricio2003@gmail.com>
2023-11-05 15:00:32 -0600

    subiendo libros basicos

--- JS/funcionalidad.js
+++ JS/funcionalidad.js
@@ -1,24 +1,315 @@
 let libros = [
+
     {
         titulo: "El señor de los anillos",
         autor: "J R R Tolkien",
         genero: "Fantasia",
-        año: 1947,
-        sinopsis: 'El señor de los anillos es una épica trilogía que sigue' +
-            "las aventuras de Frodo Bolsón y su grupo de compañeros en su búsqueda" +
-            "para destruir un poderoso anillo y así derrotar al malvado señor" +
-            "oscuro Sauron.Con  un mundo vasto y detallado, personajes memorables" +
-            "y temas profundos sobre la amistad, la valentía y la lucha entre el" +
-            "bien y el mal, esta obra maestra de la fantasía ha dejado una marca" +
-            "perdurable en la literatura y la cultura popular"
+        año:"1947",
+        sinopsis:"El señor de los anillos es una épica trilogía que sigue las aventuras de Frodo Bolsón y su grupo de"+
+            "compañeros en su búsquedapara destruir un poderoso anillo y así derrotar al malvado señor oscuro Sauron."+
+            "Con un mundo vasto y detallado,personajes memorables y temas profundos sobre la amistad, la valentía "+
+            "y la lucha entre el bien y el mal, esta obra maestrala fantasía ha dejado unamarca perdurable"+
+            "en la literatura y la cultura popular."
+    },
+
+    {
+        titulo:"El nombre del viento",
+        autor:"Patrick Rothfuss",
+        genero:"Fantasia",
+        año:"2007",
+        sinopsis:"El nombre del viento es la primera entrega de la trilogía Crónica del asesino de reyes La historia"+
+            "sigue a Kvotheun joven músico y mago en ciernes que narra sus aventuras desde su infancia en una troupe"+
+            "de artistas itinerantes hastasu vida como estudiante en la Universidad, donde aprende magia y ciencia."+
+            "Con una prosa exquisitay un mundo construido, esta novela cautiva a los lectores con su narrativa"+
+            "y personajes memorables.",
+
+    },
+
+    {
+        titulo:"Juego de tronos",
+        autor:"George R.R. Martin",
+        genero:"Fantasia",
+        año:"1996",
+        sinopsis:"Juego de tronos es el primer libro de la serie Canción de hielo y fuego Ambientada en un mundo "+
+            "ficticio inspirado en la Edad Media, la trama se centra en las luchas de poder entre diversas casas nobles"+
+            "por el control del Trono de Hierroy el dominio de los Siete Reinos. Con giros inesperados, personajes "+
+            "complejos y un entorno rico en detalles, esta obra ha cautivado a millones de lectores en todo el mundo."
+
+    },
+        //*--------------------------------------------------------------
+    {
+        titulo:"1984",
+        autor:"George Orwell",
+        genero:"Ficcion",
+        año:"1949 ",
+        sinopsis:"En un mundo distópico dominado por un gobierno totalitario, 1984 sigue la historia de Winston Smith,"+
+            "un hombre que desafía las normas impuestas por el Partido y su líder, el Gran Hermano. Orwell ofrece una "+
+            "crítica profunda de la vigilancia estatal, el control político y la manipulación de la verdad, convirtiendo "+
+            "a esta obra en un clásico atemporal de la literatura distópica."
+
     },
     {
-        titulo: "1984",
-        autor: "George Orwell",
-        genero: "Ficcion",
-        año: 1949,
-        sinopsis: ""
-    }
+        titulo:"El gran Gatsby",
+        autor:"F. Scott Fitzgerald",
+        genero:"Ficcion",
+        año:"1925",
+        sinopsis:" Ambientada en la próspera era de los años 20, El gran Gatsby narra la historia de Jay Gatsby,"+
+            "un enigmático millonario obsesionado con el amor de su vida, Daisy Buchanan. A través de la narrativa "+
+            "de Fitzgerald, se exploran temas como el sueño americano, la riqueza, el amor y la tragedia, pintando "+
+            "un retrato vívido de la decadencia de la sociedad y los sueños inalcanzables."
+
+    },
+    {
+        titulo:"Cien años de soledad",
+        autor:"Gabriel García Márquez",
+        genero:"Ficcion",
+        año:"1967",
+        sinopsis:"Cien años de soledad es una novela que sigue la historia de la familia Buendía a lo largo de varias"+
+            "generaciones en el ficticio pueblo de Macondo. Con elementos de realismo mágico, García Márquez teje "+
+            "un relato que abarca temas como la soledad, el amor, la guerra y el destino, mientras que también ofrece"+
+            "una crítica social y política de América Latina. Esta obra maestra ha dejado una profunda huella en la"+
+            "literatura mundial."
+
+    },
+        //*--------------------------------------------------------------
+    {
+        titulo:"El arte de la pintura",
+        autor:"H. W. Janson",
+        genero:"Arte",
+        año:"1994",
+        sinopsis:" Este libro presenta una amplia visión general de la evolución de la pintura occidental, desde sus "+
+            "raíces en el arte antiguo y medieval hasta las corrientes contemporáneas. Ofrece una detallada exploración"+
+            "de las técnicas, estilos y movimientos artísticos clave que han dado forma al arte de la pintura a lo largo"+
+            "de los siglos."
+
+    },
+
+    {
+        titulo:"Historia del arte",
+        autor:"E. H. Gombrich",
+        genero:"Arte",
+        año:"1950",
+        sinopsis:" Esta obra clásica es una introducción accesible y comprensiva a la historia del arte occidental."+
+            "Con un enfoque didáctico y una narrativa atractiva, Gombrich presenta de manera vívida los desarrollos"+
+            "culturales, estilísticos y técnicos que han definido la evolución del arte desde la prehistoria hasta "+
+            "el siglo XX."
+
+    },
+    {
+        titulo:"El mundo del arte",
+        autor:"Lawrence Gowing",
+        genero:"Arte",
+        año:"1994",
+        sinopsis:"El mundo del arteproporciona una visión profunda y perspicaz del arte europeo desde el"+
+            "Renacimiento hasta el siglo XX. Con un enfoque particular en la relación entre el arte y su contexto"+
+            "histórico, social y cultural, Gowing ofrece una exploración detallada de las obras maestras y los "+
+            "movimientos artísticos más influyentes de la historia."
+
+    },
+        //*--------------------------------------------------------------
+    {
+        titulo:"Geografía del mundo",
+        autor:"Susan L. Cutter",
+        genero:"Geografia",
+        año:"2015",
+        sinopsis:"Geografía del mundo ofrece una perspectiva integral de los patrones espaciales y los procesos que moldean"+
+            "el mundo actual. Desde la geografía física hasta la humana, este libro proporciona una exploración"+
+            "detallada de los fenómenos geográficos clave, como el cambio climático, la urbanización y la globalización,"+
+            "y su impacto en la humanidad y el medio ambiente."
+
+    },
+    {
+
+        titulo:"Introducción a la geografía humana",
+        autor:"James M. Rubenstein",
+        genero:"Geografia",
+        año:"2017",
+        sinopsis:"Este libro proporciona una introducción accesible a la geografía humana, centrándose en la relación "+
+            "entre el ser humano y el entorno. Explora temas como la migración, la cultura, la economía y la política"+
+            "desde una perspectiva geográfica, brindando una comprensión profunda de cómo los procesos humanos y naturales"+
+            "interactúan para dar forma al mundo en que vivimos."
+
+    },
+    {
+        titulo:"Geografía regional del mundo",
+        autor:"Lydia Mihelic Pulsipher y Alex Pulsipher",
+        genero:"Geografia",
+        año:"2015",
+        sinopsis:"Este libro proporciona un análisis detallado de las regiones geográficas clave en todo el mundo,"+
+            "explorando sus características físicas, humanas y culturales distintivas. Desde América del Norte "+
+            "hasta África, Asia y más allá, esta obra ofrece una comprensión global de las complejidades y diversidades"+
+            "geográficas que definen nuestro planeta en constante cambio."
+            
+    },
+        //*--------------------------------------------------------------
+
+    {
+        titulo:"Historia de las religiones del mundo",
+        autor:"David S Noss",
+        genero:"Religion",
+        año:"2008",
+        sinopsis:"Historia de las religiones del mundo ofrece una visión amplia y detallada de las principales"+
+            "tradiciones religiosas, desde el hinduismo y el budismo hasta el cristianismo, el islam y más allá."+
+            "Explora el desarrollo histórico, las prácticas espirituales y las creencias fundamentales de estas"+
+            "tradiciones, proporcionando una comprensión profunda de la diversidad religiosa a lo largo del"+
+            "tiempo y el espacio."
+
+    },
+    {
+        titulo:"El mundo de las religiones",
+        autor:"Ninian Smart",
+        genero:"Religion",
+        año:"1998",
+        sinopsis:"El mundo de las religiones Ninian Smart ofrece una visión global y comparativa de las principales"+
+            "tradiciones religiosas del mundo. Explora las dimensiones filosóficas, sociológicas y prácticas de las"+
+            "religiones, examinando sus doctrinas, rituales y su impacto en la vida espiritual y cultural de las"+
+            "comunidades en todo el mundo."
+
+    },
+    {
+        titulo:"Diccionario de religiones",
+        autor:"John R. Hinnells",
+        genero:"Religion",
+        año:"1997",
+        sinopsis:"Este diccionario exhaustivo ofrece una amplia cobertura de las religiones del mundo, proporcionando"+
+            "información detallada sobre sus orígenes, enseñanzas, prácticas y figuras clave. Desde las religiones"+
+            "antiguas hasta las contemporáneas, el libro ofrece una referencia completa para aquellos interesados "+
+            "en explorar la diversidad y complejidad de las creencias religiosas en todo el mundo."
+ 
+    },
+        //*--------------------------------------------------------------
+    {
+        
+        titulo:"Harry Potter y la piedra filosofal",
+        autor:"J.K. Rowling",
+        genero:"Infantiles y juveniles",
+        año:"1997",
+        inopsis:"El primer libro de la famosa serie de Harry Potter sigue las aventuras del joven mago Harry Potter y sus amigos en la escuela"+
+            "de magia y hechicería de Hogwarts. Con una combinación única de magia, amistad y valentía, este libro ha cautivado a millones"+
+            "de lectores jóvenes en todo el mundo y se ha convertido en un clásico moderno de la literatura juvenil."
+
+    },
+
+    {
+        titulo:"El principito",
+        autor:"Antoine de Saint-Exupéry",
+        genero:"Infantiles y juveniles",
+        año:"1943",
+        sinopsis:"es un cuento clásico que narra las aventuras de un niño proveniente de un pequeño planeta y su viaje por varios mundos"+
+            "en el universo. Con su exploración de temas como la amistad, el amor y la importancia de ver más allá de las apariencias,"+
+            "este libro ha dejado una profunda impresión en lectores de todas las edades en todo el mundo.,"
+
+    },
+
+    {
+        titulo:"Matilda",
+        autor:" Roald Dahl",
+        genero:"Infantiles y juveniles",
+        año:"1988",
+        sinopsis: "Matilda es la historia de una niña prodigio con una inteligencia excepcional y habilidades telequinéticas. "+
+            "A pesar de la indiferencia de sus padres y la crueldad de la directora de su escuela, Matilda encuentra consuelo en los "+
+            "libros y descubre el poder de la amistad y la valentía. Con su humor y su mensaje inspirador, este libro ha deleitado "+
+            "a lectores jóvenes en todo el mundo."
+
+    },
+        //*--------------------------------------------------------------
+    {
+        titulo:"El origen de las especies",
+        autor:"Charles Darwin",
+        genero:"Ciencias sociales",
+        año:"1859",
+        sinopsis:"El origen de las especies es una obra fundamental en la teoría de la evolución. Charles Darwin presenta su teoría de la evolución"+
+            "a través de la selección natural, revolucionando la comprensión de cómo las especies se desarrollan y se adaptan a su entorno."
+
+    },
+
+    {
+        titulo:"El Capital",
+        autor:"Karl Marx",
+        genero:"Ciencias sociales",
+        año:"1867",
+        sinopsis:"El Capital es una obra clave en la teoría marxista y la economía política. Karl Marx analiza la estructura del sistema capitalista, "+
+            "la explotación de la clase trabajadora y las implicaciones socioeconómicas de la producción y la propiedad."
+
+    },
+
+    {
+        titulo:"El choque de civilizaciones y la reconfiguración del orden mundial",
+        autor:"Samuel P. Huntington",
+        genero:"Ciencias sociales",
+        año:"1996",
+        sinopsis:"En este libro, Samuel P. Huntington argumenta que el mundo pos-Guerra Fría se caracteriza por conflictos culturales y religiosos "+
+            "entre civilizaciones en lugar de conflictos ideológicos. Propone un nuevo marco para comprender las tensiones globales y los desafíos"+
+            "geopolíticos del siglo XXI."
+
+    },
+          //*--------------------------------------------------------------
+    {
+        titulo:"El diseño de la interfaz de usuario",
+        autor:"Ben Shneiderman",
+        genero:"Ciencias Tecnologicas",
+        año:"2010",
+        sinopsis:"En este libro, Ben Shneiderman explora los principios y prácticas fundamentales para el diseño efectivo de interfaces de usuario en software "+
+            "y sistemas interactivos. Ofrece pautas claras y ejemplos prácticos para mejorar la usabilidad y la experiencia del usuario en una variedad "+
+            "de entornos tecnológicos."
+
+    },
+
+    {
+        titulo:"Handbook of Agile Software Craftsmanship",
+        autor:"Robert C. Martin",
+        genero:"Ciencias Tecnologicas",
+        año:"2008",
+        sinopsis:"Clean Code es una guía fundamental para el desarrollo de software de alta calidad. Robert C. Martin aborda principios y prácticas de programación"+
+            "que fomentan la legibilidad, la mantenibilidad y la eficiencia del código, proporcionando a los desarrolladores pautas claras para mejorar "+
+            "su proceso de codificación."
+
+    },
+
+    {
+        titulo:"Inteligencia Artificial: Un enfoque moderno",
+        autor:"Stuart Russell",
+        genero:"Ciencias Tecnologicas",
+        año:"2002",
+        sinopsis:"Este libro ofrece una visión completa y actualizada de la inteligencia artificial, abordando temas como la resolución de problemas,"+
+            "el aprendizaje automático, la percepción y la comprensión del lenguaje natural. Con ejemplos prácticos y teorías fundamentales, "+
+            "los autores presentan los conceptos clave de la inteligencia artificial de manera accesible y comprensible."
+
+    },
+        //*--------------------------------------------------------------
+    {
+        titulo:"Don Quijote de la Mancha",
+        autor:"Miguel de Cervantes",
+        genero:"Literatura clasica",
+        año:"1605 (primera parte) y 1615 (segunda parte)",
+        sinopsis: "Don Quijote de la Mancha narra las aventuras de un hidalgo enloquecido por los libros de caballería, que se embarca en una serie "+
+            "de absurdas y cómicas aventuras junto a su fiel escudero Sancho Panza. Considerada una de las obras más destacadas de la literatura española"+
+            " }y mundial, la novela de Cervantes es una sátira ingeniosa y conmovedora de la sociedad y la naturaleza humana."
+
+    },
+
+    {
+        titulo:"Ogullo y prejuicio",
+        autor:"Jane Austen",
+        genero:"Literatura clasica",
+        año:"1813",
+        sinopsis:"Orgullo y prejuicio sigue la historia de Elizabeth Bennet y su relación con el rico y orgulloso Sr. Darcy en la Inglaterra rural"+
+            "el siglo XIX. Con una prosa ágil y aguda, Jane Austen ofrece una crítica mordaz de la sociedad de su época y una exploración profunda "+
+            "de temas como el matrimonio, el amor y la clase social."
+
+    },
+
+    {
+        titulo:"Crimen y castigo",
+        autor:"Fyodor Dostoevsky",
+        genero:"Literatura clasica",
+        año:"1866",
+        sinopsis:"Crimen y castigo cuenta la historia de Rodion Raskolnikov, un estudiante pobre que comete un asesinato y lucha con su culpa y su conciencia."+
+            "Ambientada en la Rusia del siglo XIX, la novela de Dostoevsky explora cuestiones de moralidad, redención y la psicología humana,"+
+            "ofreciendo una poderosa reflexión sobre el sufrimiento y la redención."
+
+    },
+ 
 ];
 
 //*--------------------------------------------------------------
